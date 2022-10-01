af3580da7617fd0b2451d02c549b46d5ef7374c7
ArthurH-20 <81658735+Arthur-H15@users.noreply.github.com>
2022-10-01 09:03:09 -0300

    up1

---
+++ .docker/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ ! -f ".env" ]; then
+cp .env.example .env
+fi
+
+npm i
+npm run start:dev
---
+++ src/routes/routes.controller.ts
@@ -0,0 +1,79 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Inject, OnModuleInit } from '@nestjs/common';
+import { RoutesService } from './routes.service';
+import { CreateRouteDto } from './dto/create-route.dto';
+import { UpdateRouteDto } from './dto/update-route.dto';
+import { ClientKafka, MessagePattern, Payload } from '@nestjs/microservices';
+import { Producer } from '@nestjs/microservices/external/kafka.interface';
+import { RoutesGateway } from './routes.gateway';
+import { createHistogram } from 'perf_hooks';
+
+
+@Controller('routes')
+export class RoutesController implements OnModuleInit {
+ private kafkaProducer:Producer;
+  constructor(
+    private readonly routesService: RoutesService,
+    @Inject('KAFKA_SERVICE')
+    private kafkaClient:ClientKafka,
+    private routesGateway:RoutesGateway
+    ) {}
+
+  @Post()
+  create(@Body() createRouteDto: CreateRouteDto) {
+    return this.routesService.create(createRouteDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.routesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.routesService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateRouteDto: UpdateRouteDto) {
+    return this.routesService.update(+id, updateRouteDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.routesService.remove(+id);
+  }
+  async onModuleInit() {
+    this.kafkaProducer= await this.kafkaClient.connect();
+    
+  }
+  @Get(':id/start')
+  startRoute(@Param('id') id:string){
+    console.log("aqui")
+    this.kafkaProducer.send(
+      {
+        topic:'route.new-direction',
+        messages:[
+          {
+            key:'route.new-direction',
+            value:JSON.stringify({routeId:id,clienteId:''})
+          }
+        ]
+      }
+    )
+  }
+  @MessagePattern('route.new-position')
+  consumeNewPosition(
+    @Payload()
+    message: {
+      value: {
+        routeId: string;
+        clientId: string;
+        position: [number, number];
+        finished: boolean;
+      };
+    },
+  ) {
+    // console.log({value:message.value})
+    this.routesGateway.sendPosition(message.value);
+  }
+}
